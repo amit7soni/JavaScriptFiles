3a6267d9428a6afaa1d4d9be36eeac8e0ed143d2
amit7soni <amit.kishoreda@gmail.com>
2013-02-11 15:21:52 +0530

    Update jQuery.newsTicker.js
    NewsTicker Like Facebook

--- jQuery.newsTicker.js
+++ jQuery.newsTicker.js
@@ -1,3 +1,22 @@
 (function($){
-  
+  $.fn.newsTicker = function(options){
+    var defaults = {
+      scrollSpeed:1000,
+      easingType:'easeInOutQuint',
+      delayTime:0,
+      scrollInterval:5000
+    },
+    
+    settings = $.extend({},defaults,options),
+    $this = $(this);
+    
+    setInterval(function(){
+      return $this.children('li:first').delay(settings.delayTime).animate({
+        marginTop:'-' + $(this).css('height'),
+        opacity:'hide'
+        },settings.scrollSpeed,settings.easingType,function(){
+          $this.children('li:first').appendTo($this).css('margin-top','0px').fadeIn(500);
+        });
+    },settings.scrollInterval)
+  }
 })(jQuery);
