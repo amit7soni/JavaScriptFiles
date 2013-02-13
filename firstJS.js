10f14e09d05655b2fe9622b79ae44e65d4b7fb76
amit7soni <amit.kishoreda@gmail.com>
2013-02-13 13:26:33 +0530

    Update firstJS.js

--- firstJS.js
+++ firstJS.js
@@ -1,3 +1,12 @@
 $(function(){
-  alert("hello");
+  //alert("hello");
+  $('h1').hover(function(){
+    
+    $(this).css('color','#0f0')
+    
+  },function(){
+    
+    $(this).css('color','#f00')
+    
+  })
 });
